094e37111bb54940227eccf40ba89fbb783c0c5d
thanhiep <buithanhhiep.ck@gmail.com>
2024-07-01 00:06:38 +0700

    deploy

--- src/modules/User/ProfilePage/index.tsx
+++ src/modules/User/ProfilePage/index.tsx
@@ -3,23 +3,30 @@ import "./style.css";
 import { getUserProfileApi } from "../../../apis/user";
 import { Col, Row, Skeleton } from "antd";
 import Course from "./course";
-import avatarIcon from "./../../../assets/img/avartarIcon.jpg"
+import avatarIcon from "./../../../assets/img/avartarIcon.jpg";
+import { useAppSelector } from "../../../redux/hook";
+import { useNavigate } from "react-router-dom";
 
 export default function ProfilePage() {
+  const navigate = useNavigate();
+
+  const { currentUser } = useAppSelector((state) => state.user);
+  if (!currentUser) {
+    navigate("/");
+  }
+
   const { data, isLoading } = useQuery({
     queryKey: ["profile"],
     queryFn: () => getUserProfileApi(),
   });
 
   const renderCourse = () => {
-    if(data){
-      return data.chiTietKhoaHocGhiDanh.map((course)=>{
-        return (
-          <Course key={course.maKhoaHoc} course={course}/>
-        )
-      })
+    if (data) {
+      return data.chiTietKhoaHocGhiDanh.map((course) => {
+        return <Course key={course.maKhoaHoc} course={course} />;
+      });
     }
-  }
+  };
 
   const renderProfile = () => {
     if (data) {
@@ -30,25 +37,30 @@ export default function ProfilePage() {
               <h3>Thông tin tài khoản</h3>
               <img src={avatarIcon} alt="avartar" />
               <div className="profileTextBox">
-              <div className="profileText">
-                <p><span>Họ tên: </span> {data.hoTen}</p>
-                <p><span>Tài khoản: </span> {data.taiKhoan}</p>
-                <p><span>Email: </span> {data.email}</p>
-                <p><span>Điện thoại: </span> {data.soDT}</p>
+                <div className="profileText">
+                  <p>
+                    <span>Họ tên: </span> {data.hoTen}
+                  </p>
+                  <p>
+                    <span>Tài khoản: </span> {data.taiKhoan}
+                  </p>
+                  <p>
+                    <span>Email: </span> {data.email}
+                  </p>
+                  <p>
+                    <span>Điện thoại: </span> {data.soDT}
+                  </p>
+                </div>
               </div>
-              </div>
-              
             </div>
           </Col>
           <Col xs={24} md={16}>
-          <div className="profileContent">
-            <h3>Khóa học của bạn</h3>
-            <div className="profileCourse">
-            <Row gutter={40}>
-              {renderCourse()}
-            </Row>
+            <div className="profileContent">
+              <h3>Khóa học của bạn</h3>
+              <div className="profileCourse">
+                <Row gutter={40}>{renderCourse()}</Row>
+              </div>
             </div>
-          </div>
           </Col>
         </Row>
       );
@@ -64,9 +76,8 @@ export default function ProfilePage() {
         <p>Các khóa học bạn đã đăng ký</p>
       </div>
       <div className="container mx-auto profileContentBox">
-      {renderProfile()}
+        {renderProfile()}
       </div>
-      
     </div>
   );
 }
